1335e29613d3f87b4047c5934aa531f12aec9c73
RitikDua <rittikdua@gmail.com>
2020-07-23 13:30:34 +0530

    user model

---
+++ backend/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {//username
+    type: String,
+    unique:true,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+userSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
+
+userSchema.methods.validPassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function () {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000, 10),
+  }, process.env.JWT_SECRET);
+};
+
+mongoose.model('User', userSchema);
